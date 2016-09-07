4ebdefeae3bc5c83024205602178d0c2059c0e32
skinzor <robertpopescu95@yahoo.com>
2016-09-07 20:58:04 +0300

    update

--- app/build/generated/source/r/debug/android/support/design/R.java
+++ app/build/generated/source/r/debug/android/support/design/R.java
@@ -642,8 +642,8 @@ public final class R {
 		public static final int design_fab_background = 0x7f020052;
 		public static final int design_ic_visibility = 0x7f020053;
 		public static final int design_snackbar_background = 0x7f020054;
-		public static final int navigation_empty_icon = 0x7f02006a;
-		public static final int notification_template_icon_bg = 0x7f020070;
+		public static final int navigation_empty_icon = 0x7f02006d;
+		public static final int notification_template_icon_bg = 0x7f020073;
 	}
 	public static final class id {
 		public static final int action0 = 0x7f100097;
